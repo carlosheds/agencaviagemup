1d75781ffbc2307e65bf708089224650444158a6
carlosheds <carlos.heds@gmail.com>
2022-02-07 15:54:07 -0300

    App.js finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Menu from './components/Menu'
+import Home from './views/Home';
+import Clientes from './views/Clientes';
+import Destinos from './views/Destinos';
+import Passagens from './views/Passagens';
+import Promocoes from './views/Promocoes';
+import Contato from './views/Contato';
+import Footer from './components/Footer';
+import Privacy from './views/Privacy';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Menu />
+        <div className="container-fluid-global">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/clientes" element={<Clientes />} />
+            <Route path="/destinos" element={<Destinos />} />
+            <Route path="/passagens" element={<Passagens />} />
+            <Route path="/promocoes" element={<Promocoes />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/politica-de-privacidade" element={<Privacy />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
